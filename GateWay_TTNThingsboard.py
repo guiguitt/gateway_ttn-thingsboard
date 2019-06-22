2c6360a1722daeaf391f15525d4480a6e9b1e5f8
Guiguitant Théo <theo.guiguitant@gmail.com>
2019-06-22 11:42:19 +0200

    add comment

--- GateWay_TTNThingsboard.py
+++ GateWay_TTNThingsboard.py
@@ -3,14 +3,20 @@
 Théo Guiguitant 
 theo.guiguitant@gmail.com
 """
+
 import paho.mqtt.client as mqtt_client
 from Lib import *
 
-printlog("lancement du script Gateway TTN vers Thingsboard")
-dPrm = getIniParameters("commissioning.ini")
-client = mqtt_client.Client( client_id=dPrm['MQTT']['clientid'] )
+printlog("Script Gateway TTN to Thingsboard has begin")
+
+
+dPrm = getIniParameters("commissioning.ini") #loading of all keys and connection parameters
+
+
+client = mqtt_client.Client( client_id=dPrm['MQTT']['clientid'] ) #launch of the mqtt client
 printlog("Client MQTT begin ")
-# Assignation des fonctions de rappel
+
+#interrupt function
 client.on_message = on_message
 client.on_connect = on_connect
 #client.on_log = on_log 
@@ -18,7 +24,8 @@ client.on_connect = on_connect
 client.username_pw_set(username=dPrm['TTN']['appid'], password=dPrm['TTN']['accesskey'] )
 client.connect(host=dPrm['MQTT']['broker'], port=int(dPrm['MQTT']['port']), keepalive=int(dPrm['MQTT']['keep_alive']))
 printlog("Client MQTT connect to TTN")
+printlog("waiting for a communication")
 client.subscribe( "+/devices/+/up" )
 
-# Envoi des messages
+# standby loop
 client.loop_forever()
