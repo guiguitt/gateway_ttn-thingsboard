eee84ffb8ec5464cc9a2a22ea51aaaa081eab35a
DDorch <dorch@laposte.net>
2019-07-26 13:51:04 +0200

    Improvement of error management

--- gateway_ttn_thing.py
+++ gateway_ttn_thing.py
@@ -10,9 +10,9 @@ makes http requests to the Things Boards system API
 import paho.mqtt.client as mqtt_client
 from library import *
 
-printlog("Script Gateway TTN to Thingsboard has begin")
+printlog("Start Gateway TTN to Thingsboard")
 
-
+printlog("Loading configuration in commissioning.ini")
 dPrm = getIniParameters('commissioning.ini') #loading of all keys and connection parameters
 
 for key, value in dPrm.items():
@@ -20,22 +20,19 @@ for key, value in dPrm.items():
             if value=="":
                 sys.exit("Error message: commissioning incomplete")
 
-printlog("charge of commissioning")
-
+printlog("1/3 - Start MQTT Client")
 client = mqtt_client.Client( client_id=dPrm['MQTT']['clientid'] ) #launch of the mqtt client
-
-printlog("1/3 - Client MQTT begin ")
 
 #interrupt function
 client.on_message = on_message
 client.on_connect = on_connect
 #client.on_log = on_log 
 # Connexion broker
-
 client.username_pw_set(username=dPrm['TTN']['appid'], password=dPrm['TTN']['accesskey'] )
+
 while True:
     try:
-        printlog("2/3 - connect in progress...")
+        printlog("2/3 - Connection in progress...")
         client.connect(host=dPrm['MQTT']['broker'], port=int(dPrm['MQTT']['port']), keepalive=int(dPrm['MQTT']['keep_alive']))
         break
     except TimeoutError as e:
@@ -46,7 +43,6 @@ while True:
         printlog("OSError: (Probably: not permission to open port Mqtt)")
         printlog(str(e))
         continue
-    
 
 client.subscribe(dPrm['MQTT']['topic'])
 printlog("3/3 - Client MQTT is connected with TTN broker")
--- library.py
+++ library.py
@@ -6,7 +6,7 @@ import sys
 def getIniParameters(sFile):
     """
     Read parameters in ini file
-    @param sFile Paht to the ini file to read
+    @param sFile Path to the ini file to read
     @return Dictionary with sections and parameters contained in the INI file
     """
     import os,sys
@@ -30,59 +30,74 @@ def printlog(s, sep = ": "):
     sDateTime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     print(sDateTime + sep + s)
     sys.stdout.flush()
-    
+
 def PostThingsboard( JsDATA, DeviceId):
     """
-    performs queries on the Things Board API to be able to post the passed data as a parameter. 
+    performs queries on the Things Board API to be able to post the passed data as a parameter.
         1. it requests token access with the publicId
-        2. it requests the device token with access token and the deviceID pass as parameter  
+        2. it requests the device token with access token and the deviceID pass as parameter
         3. it posts the data to pass as a parameter with the device token
         @param dictionary: that contains the name of the data and the data
                DeviceID: the name of the device on TTN and the device ID on Thingsboard
-
     """
     dPrm = getIniParameters('commissioning.ini')
-    headers_AcessT = {
+    headers_AccessT = {
     'content-type': "application/json",
     'accept': "application/json"
     }
-    printlog("1/9 - send request to Acess Token")
-    ResponseAcessToken = requests.post(dPrm['THINGS']['url']+'/api/auth/login/public', headers=headers_AcessT, data='{"publicId":"'+ dPrm['THINGS']['publicid']+'"}')
-    if 'status' in ResponseAcessToken.text: 
-        printlog(ResponseAcessToken.text)
-    else:
-        printlog("2/9 - Request response ok")
+    printlog("1/6 - Send request to Access Token")
 
-        
-    
-    AcessToken=eval(ResponseAcessToken.text)['token']
-    printlog("3/9 - ***done***")
-    
+    try:
+        ResponseAccessToken = requests.post(dPrm['THINGS']['url']+'/api/auth/login/public', headers=headers_AccessT, data='{"publicId":"'+ dPrm['THINGS']['publicid']+'"}')
+    except requests.exceptions.RequestException as e:
+        printlog("Error " + e)
+        return
+
+    if 'status' in ResponseAccessToken.text:
+        printlog(ResponseAccessToken.text)
+        return
+    else:
+        printlog("2/6 - Request response ok")
+
+    try:
+        AccessToken=eval(ResponseAccessToken.text)['token']
+    except:
+        printlog("Error retrieving token in " + ResponseAccessToken.text)
+        return
+
     headers_DeviceT = {
             'Accept': 'application/json',
-            'X-Authorization': "Bearer " +AcessToken
+            'X-Authorization': "Bearer " +AccessToken
             }
-    printlog("4/9 - send request to Device token")
-    ResponseDeviceToken = requests.get(dPrm['THINGS']['url']+'/api/device/'+DeviceId+'/credentials', headers=headers_DeviceT)
+    printlog("3/6 - Send request to Device token")
+    try:
+        ResponseDeviceToken = requests.get(dPrm['THINGS']['url']+'/api/device/'+DeviceId+'/credentials', headers=headers_DeviceT)
+    except requests.exceptions.RequestException as e:
+        printlog("Error " + e)
+        return
+
     if 'status' in ResponseDeviceToken.text:
-        printlog(ResponseAcessToken.text)
+        printlog(ResponseAccessToken.text)
+        return
     else:
-        printlog("5/9 - Request response ok")
+        printlog("4/6 - Request response ok")
 
-    
-    
     null = None
-    DeviceToken=str(eval(ResponseDeviceToken.text)['credentialsId'])
-    printlog("6/9 - ***done***")
-    
-    
 
-    printlog("7/9 - send request telemetry" )
-    r = requests.post(dPrm['THINGS']['url']+'/api/v1/'+DeviceToken+'/telemetry', json=JsDATA)
-    printlog("8/9 - request code reponse:"+str(r.text))
-    if 'status' in ResponseDeviceToken.text:
-        printlog( r.text)
-    printlog("9/9 - ***done***") 
+    try:
+        DeviceToken=str(eval(ResponseDeviceToken.text)['credentialsId'])
+    except:
+        printlog("Error retrieving credentials Id")
+        return
+
+    printlog("5/6 - Send request telemetry")
+    try:
+        r = requests.post(dPrm['THINGS']['url']+'/api/v1/'+DeviceToken+'/telemetry', json=JsDATA)
+    except requests.exceptions.RequestException as e:
+        printlog("Error " + e)
+        return
+
+    printlog("6/6 - Request code reponse:"+str(r.text))
 
 
 def on_log( client, userdata, level, buf ):
@@ -94,15 +109,15 @@ def on_connect( client, userdata, flags, rc ):
 
 def on_message( client, userdata, message):
     """
-    application that processes data when a message is 
+    application that processes data when a message is
     received and starts the post procedure on ThingsBoard
     """
     printlog( "Recept message MQTT..." )
     printlog( "Topic : "+ str(message.topic) )
     msg = json.loads(message.payload.decode("utf-8" ))
     #printlog("dev_if : " +y["dev_id"])
-    #printlog("Latitude: " +y["payload_fields"]["latitude"])    
-    #printlog("Longitude: " +y["payload_fields"]["longitude"])    
-    #printlog("detection: %s" +y["payload_fields"]["detection"])	
+    #printlog("Latitude: " +y["payload_fields"]["latitude"])
+    #printlog("Longitude: " +y["payload_fields"]["longitude"])
+    #printlog("detection: %s" +y["payload_fields"]["detection"])
     printlog("data: "+str(msg["payload_fields"]))
     PostThingsboard(msg["payload_fields"], msg['dev_id'])
